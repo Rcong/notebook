9a4f976fcacb8af781be9828a0f73851ae616e7d
朱超然 <zhuchaoran@raycloud.com>
2017-12-05 15:12:04 +0800

    :sparkles:

--- Code/learn-webpack/2.entry/webpack.config.js
+++ Code/learn-webpack/2.entry/webpack.config.js
@@ -1,8 +1,8 @@
 let path = require('path');
 
 module.exports = {
-    entry: {
-        bundle1: './src/index.js',
+    entry: {//多个入口打包,参考pc的模式,具体到某一个某块,再去加载某些js的时候。
+        bundle1: './src/index1.js',
         bundle2: './src/index2.js',
         bundle3: './src/index3.js',
         bundle4: './src/index4.js',
