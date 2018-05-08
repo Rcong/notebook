f186aeafced8b2329a0a40f2c9d1f420aa00b903
朱超然 <zhuchaoran@raycloud.com>
2018-05-08 09:58:33 +0800

    demo

--- Code/learn-redux/middleware/src/index.js
+++ Code/learn-redux/middleware/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Count from './component/Count'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import MovieList from './component/MovieList'
 import reducers from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers)
+
+
+import store from 'stores';
 
 const render = () => ReactDOM.render(
-  <Count
+  <MovieList
     value={store.getState()}
     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
