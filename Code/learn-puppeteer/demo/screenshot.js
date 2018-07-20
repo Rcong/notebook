f144df8266eedbf6e5f0cf90830fcd421743ce62
朱超然 <zhuchaoran@raycloud.com>
2018-07-20 15:11:46 +0800

    screenshot

--- Code/learn-puppeteer/demo/screenshot.js
+++ Code/learn-puppeteer/demo/screenshot.js
@@ -1,22 +1,43 @@
 let puppeteer = require('puppeteer');
+let inquirer = require('inquirer');
 let http = require('http');
 let fs = require('fs');
 
 (async() => {
+
+    console.info('请填网页地址')
+    let search = await inquirer.prompt([{
+        type: 'input',
+        name: 'url',
+        message: '请填网页地址:',
+        validate: function (input) {
+            let done = this.async()
+            if (!input) return done('请填网页地址')
+            return done(null, true)
+        }
+    }]);
+    console.info('搜索中..');
+    await printScreen(search.url);
+
+})();
+
+async function printScreen(url){
+
     let browser = await puppeteer.launch({
         executablePath: './chromium/Chromium.app/Contents/MacOS/Chromium',
         ignoreHTTPSErrors: true,
         headless: false,
     });
     let page = await browser.newPage();
-    await page.goto('http://www.ruanyifeng.com/road/');
+    await page.goto(url);
     // 设置浏览器视窗
     await page.setViewport({ width: 1376, height: 1000 });
     await page.screenshot({path: 'img/demo.png', fullPage: true});
     page.close();
 
     let imgPage = await browser.newPage();
-
+    await imgPage.setViewport({ width: 1376, height: 1000 });
+    
     fs.readFile('img/demo.png','binary',function(err, file) {
         if (err) {
             console.log(err);
@@ -34,4 +55,5 @@ let fs = require('fs');
     });
 
     // await browser.close();
-})();
+
+}
