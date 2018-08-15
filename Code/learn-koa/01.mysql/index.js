87c416f806c8f408d85c17cb9ff571f53d0fac58
朱超然 <654846387@qq.com>
2018-08-15 23:06:03 +0800

    :rocket:user controller添加各种方法

--- Code/learn-koa/01.mysql/index.js
+++ Code/learn-koa/01.mysql/index.js
@@ -18,40 +18,6 @@ app.use(async (ctx, next) => {
 
 app.use(bodyparser());
 
-// router.post('/api/createUser', async(ctx) => {
-//     let user = ctx.request.body;
-//     await User.create(user);
-//     ctx.body = user
-// });
-
-// router.get('/api/user', async ctx => {
-//     let { id } = ctx.query;
-//     ctx.body = await User.findOne({ where: { id } });
-// });
-
-// router.post('/api/editUser', async ctx => {
-//     let data = ctx.request.body;
-//     await User.update({
-//         lastName: data.lastName,
-//         firstName: data.firstName
-//     }, {
-//         where: {
-//             id: data.id
-//         }
-//     });
-//     ctx.body = await User.findOne({ where: { id: data.id } });
-// });
-
-// router.del('/api/delUser', async ctx => {
-//     let {id} = ctx.query;
-//     await User.destroy({ where: { id } });
-//     ctx.body = { status: 200 }
-// });
-
-// router.get('/api/userList', async(ctx, next) => {
-//     ctx.body = await User.findAndCountAll();
-// });
-
 app.use(router.routes());
 
 app.listen(8080 , () => {
--- Code/learn-koa/01.mysql/src/controller/user.js
+++ Code/learn-koa/01.mysql/src/controller/user.js
@@ -3,7 +3,7 @@ let router = new Router();
 let userService = require('../service/user');
 // let auth = require('../middleware/auth');
 
-router.get('/login', async ctx => {
+router.post('/login', async ctx => {
     let {name, password} = ctx.query;
     let user = await userService.login({name, password});
     if (user) {
@@ -12,6 +12,40 @@ router.get('/login', async ctx => {
     } else {
         ctx.body = 'login error';
     }
+});
+
+router.post('/api/createUser', async(ctx) => {
+    let user = ctx.request.body;
+    await User.create(user);
+    ctx.body = user
+});
+
+router.get('/api/user', async ctx => {
+    let { id } = ctx.query;
+    ctx.body = await User.findOne({ where: { id } });
+});
+
+router.post('/api/editUser', async ctx => {
+    let data = ctx.request.body;
+    await User.update({
+        lastName: data.lastName,
+        firstName: data.firstName
+    }, {
+        where: {
+            id: data.id
+        }
+    });
+    ctx.body = await User.findOne({ where: { id: data.id } });
+});
+
+router.del('/api/delUser', async ctx => {
+    let {id} = ctx.query;
+    await User.destroy({ where: { id } });
+    ctx.body = { status: 200 }
+});
+
+router.get('/api/userList', async(ctx, next) => {
+    ctx.body = await User.findAndCountAll();
 });
 
 // router.get('/info', auth, async ctx => {
