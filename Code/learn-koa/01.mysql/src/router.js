06a74766861f712626b6f7aebe19de001753feee
朱超然 <654846387@qq.com>
2018-09-10 09:24:56 +0800

    :sparkles:

--- Code/learn-koa/01.mysql/src/router.js
+++ Code/learn-koa/01.mysql/src/router.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 const userCtrl = require('./controller/user');
+const verifyUser = require('./middleware/auth');
 const router = new Router();
 
-// router.post('/user/login', userCtrl.login);
 
 router.use('/user', userCtrl.routes());
+router.get('/user/auth', verifyUser, ctx => ctx.resSuc());
+
 
 module.exports = router;
