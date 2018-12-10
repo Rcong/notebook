779aeddbc52327755aac50f73fbfffe98ad205aa
朱超然 <654846387@qq.com>
2018-12-10 15:36:27 +0800

    基础架构搭建

--- Code/learn-node/sdorica/src/App.js
+++ Code/learn-node/sdorica/src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import asyncComponent from '@Utils/asyncComponent';
+import { layoutContainer } from '@Components/LayoutContainer';
+
+const RoleList = asyncComponent(() => import('@Pages/RoleList'));
+const StrategyList = asyncComponent(() => import('@Pages/StrategyList'));
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/roleList" component={layoutContainer(RoleList)}/>
+                    <Route path="/strategyList" component={layoutContainer(StrategyList)}/>
+                </Switch>
+            </Router>
+        );
+    }
+
+}
+  
 export default App;
