ef35ad1edc5c73d518d0d0a3549dec6c379cabc0
朱超然 <654846387@qq.com>
2019-03-07 23:19:13 +0800

    modify

--- Code/decorator-util/output/index.js
+++ Code/decorator-util/output/index.js
@@ -8,16 +8,22 @@ exports.injectFunc = void 0;
 var injectFunc = function injectFunc(func) {
   return function (target, name, descriptor) {
     var rawFunc = descriptor.value || descriptor.initializer();
-
-    descriptor.value = function () {
+    descriptor.initializer = descriptor.initializer ? function () {
       for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
         args[_key] = arguments[_key];
       }
 
       func.call.apply(func, [this].concat(args));
       return rawFunc.apply(this, args);
-    };
+    } : descriptor.initializer;
+    descriptor.value = descriptor.value ? function () {
+      for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
 
+      func.call.apply(func, [this].concat(args));
+      return rawFunc.apply(this, args);
+    } : descriptor.value;
     return descriptor;
   };
 };
--- Code/decorator-util/src/index.js
+++ Code/decorator-util/src/index.js
@@ -2,10 +2,15 @@ export const injectFunc = func => (target, name, descriptor) => {
 
     const rawFunc = descriptor.value || descriptor.initializer();
 
-    descriptor.value = function(...args) {
+    descriptor.initializer = descriptor.initializer ? function(...args) {
         func.call(this, ...args)
         return rawFunc.apply(this, args);
-    };
+    } : descriptor.initializer;
+
+    descriptor.value = descriptor.value ? function(...args) {
+        func.call(this, ...args)
+        return rawFunc.apply(this, args);
+    } : descriptor.value;
 
     return descriptor;
 }
